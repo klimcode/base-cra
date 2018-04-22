8e64d4ee8adccefca7cf83ac4d5f646ae178974c
Evgenii Klimov <disturh@gmail.com>
2018-04-22 21:58:02 +0300

    React-stringifier is used. Changes in CSS.

--- src/components/App.js
+++ src/components/App.js
@@ -3,5 +3,6 @@ import React from 'react';
 export default () => (
   <div>
     <h2>Home component</h2>
+    {Array(1000).fill(1).map((object, i) => <span key={i}>line_{i}; </span>)}
   </div>
 );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 /* eslint-env browser */
 import React from 'react';
-// import ReactDOM from 'react-dom';
-import { render } from 'react-snapshot';
-import './css/index.css';
+import ReactDOM from 'react-dom';
+import './css/critical.css';
 import App from './components/App';
 // import registerServiceWorker from './registerServiceWorker';
 
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
 
 
